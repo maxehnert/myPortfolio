984d5d177086a466b02e4107c99aa7639ea0941d
Max Ehnert <max.ehnert@gmail.com>
2014-12-23 17:51:55 -0500

    added animation trigger by waypointjs

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
-//Navbar change color
-// $('.navbar').waypoint(function() {
-//   $('.navbar').addClass('newColor')
-// }, {
-//   offset: function() {
-//     return -$(this).height();
-//   }
-// });
-
+//tooltip function
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
+//scrolls to targeted id
 $('body').scrollspy({ target: '.navbar-fixed-top' });
+
+//waypoint for animation
+$('.cd-color-2').waypoint(function(direction) {
+  $('.img-skill').removeClass('imgnone');
+  $('.imgleft').addClass('animated bounceInLeft');
+  $('.imgcenter').addClass('animated bounceIn');
+  $('.imgright').addClass('animated bounceInRight');
+}, {
+  offset: '50%'
+});
