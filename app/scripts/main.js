ef1cb257a6732f6cff2b3e65dd1b2d1111d2f7f5
Max Ehnert <max.ehnert@gmail.com>
2016-03-17 21:11:22 -0500

    had to add hacky css reset because normalize isn't working the same yet.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,10 @@
+// require('../../node_modules/normalize.css/normalize.css');
 require('../../bower_components/looper/src/looper.css');
 require('../../node_modules/githubjs/src/github.css');
 require('../styles/animate.scss');
 require('../styles/main.scss');
 var Github = require('../../bower_components/githubjs/dist/github.min.js');
+
 /*
  * Click handler because it wasn't responding correctly with ad blockers for some reason.
 */
